77a7125c48eb8c5b6c5bf2bf56cddfd48440c6e9
flavioespinoza <flavio.espinoza@gmail.com>
2019-10-22 03:03:27 -0600

    Added subtitle prop to FlavioClass

--- src/FlavioClass.jsx
+++ src/FlavioClass.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
-export default class FlavioClass extends Component {
+class FlavioClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Hello Flavio Class Component!!! :)',
+      subtitle: undefined,
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      subtitle: this.props.subtitle,
+    });
+  }
   render() {
+    const { title, subtitle } = this.state;
+
     return (
-      <div style={{padding: 42}}>
-        <h1>Hello Flavio Class Component!!!</h1>
+      <div style={{ padding: 42 }}>
+        <h1>{title}</h1>
+        {subtitle ? <h5>{subtitle}</h5> : null}
       </div>
-    )
+    );
   }
 }
+
+export default FlavioClass;
